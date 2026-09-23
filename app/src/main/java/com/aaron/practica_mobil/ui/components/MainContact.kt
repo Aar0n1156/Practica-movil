faa32dcc7f8b3d7ae43130937fd6f48f48feb9b5
Aaron Arce <aaronarce0506@oulook.com>
2026-09-23 00:35:32 -0700

    Cambios en MainContact.kt

--- app/src/main/java/com/aaron/practica_mobil/ui/components/MainContact.kt
+++ app/src/main/java/com/aaron/practica_mobil/ui/components/MainContact.kt
@@ -1,43 +1,46 @@
 package com.aaron.practica_mobil.ui.components
 
-import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainContact (){
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
+        modifier = Modifier.fillMaxSize().statusBarsPadding(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
     ) {
-        Text(text = "Mis Contactos",
-            color = Color(0xFFFF0000),
-            fontSize = 20.sp,
+        Text("Mis Contactos",
+            color = Color(0xFF0000FF),
+            fontSize = 28.sp,
             fontWeight = FontWeight.Bold)
-        Row() {
-            Text("Cuadro de Buscar")
-            Text("Boton")
+        Row {
+            Text("Cuadro de Busqueda",
+                color = Color(0xFF000000),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold)
+            Text("      ")
+            Text(
+                "Boton", color = Color(0xFF000000),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold)
         }
-        Contact("Aaron","12346789")
-        Contact("Aaron","12346789")
-        Contact("Aaron","12346789")
-        Contact("Aaron","12346789")
-        Contact("Aaron","12346789")
+        Contact("Aaron","123456789")
+        Contact("Aaron","123456789")
+        Contact("Aaron","123456789")
+        Contact("Aaron","123456789")
+        Contact("Aaron","123465789")
     }
 
 }
